f63fd429e49172fe84641521c24e972594197833
heybeals <121995767+heybeals@users.noreply.github.com>
2023-01-28 15:17:32 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <h3>Links to my Big Assisgnments</h3>
     <ul>
      <li><a href="website/daily.php">Switch</a></li>
-     <li><a href="">Troubleshoot</a></li>
+     <li><a href="website/adder.php">Troubleshoot</a></li>
      <li><a href="">Calculator</a></li>
      <li><a href="">Email</a></li>
      <li><a href="">Database</a></li>
@@ -37,7 +37,8 @@
       <ul>  
         <li><em>Nothing to Show</em></li>
       </ul>
-     <li><h3>Week 2</h3>
+    </li>
+    <li><h3>Week 2</h3>
       <ul>  
         <li><a href="weeks/week2/var.php">var.php</a></li>
         <li><a href="weeks/week2/vars2.php">var2.php</a></li>
@@ -48,13 +49,24 @@
     </li>
     <li><h3>Week 3</h3>
       <ul>  
-      <li><a href="weeks/week3/if.php">if.php</a></li>
-      <li><a href="weeks/week3/date.php">date.php</a></li>
-      <li><a href="weeks/week3/for-each.php">for-each.php</a></li>
-      <li><a href="weeks/week3/index.php">index.php</a></li>
-      <li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
-      <li><a href="weeks/week3/switch.php">switch.php</a></li>
+        <li><a href="weeks/week3/if.php">if.php</a></li>
+        <li><a href="weeks/week3/date.php">date.php</a></li>
+        <li><a href="weeks/week3/for-each.php">for-each.php</a></li>
+        <li><a href="weeks/week3/index.php">index.php</a></li>
+        <li><a href="weeks/week3/for-loop.php">for-loop.php</a></li>
+        <li><a href="weeks/week3/switch.php">switch.php</a></li>
       </ul>
+    </li>
+    <li><h3>Week 4</h3>
+      <ul>  
+        <li><a href="weeks/week4/form-get.php">form-get.php</a></li>
+        <li><a href="weeks/week4/form1.php">form1.php</a></li>
+        <li><a href="weeks/week4/form2.php">form2.php</a></li>
+        <li><a href="weeks/week4/form3.php">form3.php</a></li>
+        <li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
+        <li><a href="weeks/week4/celsius.php">celsius.php</a></li>
+      </ul>
+    </li>
    </ol>
 </aside>
 
