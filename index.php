e8a0eade796b6f6477ca7ee0b0529492db88e9ce
heybeals <121995767+heybeals@users.noreply.github.com>
2023-02-04 20:03:08 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <ul>
      <li><a href="website/daily.php">Switch</a></li>
      <li><a href="website/adder.php">Troubleshoot</a></li>
-     <li><a href="">Calculator</a></li>
+     <li><a href="website/calculator.php">Calculator</a></li>
      <li><a href="">Email</a></li>
      <li><a href="">Database</a></li>
      <li><a href="">Gallery</a></li>
@@ -65,6 +65,13 @@
         <li><a href="weeks/week4/form3.php">form3.php</a></li>
         <li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
         <li><a href="weeks/week4/celsius.php">celsius.php</a></li>
+      </ul>
+    </li>
+    <li><h3>Week 5</h3>
+      <ul>  
+        <li><a href="weeks/week5/currency1.php">currency1.php</a></li>
+        <li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+        <li><a href="weeks/week5/currency3.php">currency3.php</a></li>
       </ul>
     </li>
    </ol>
